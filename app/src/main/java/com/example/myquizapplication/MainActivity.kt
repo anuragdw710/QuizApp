97d4403eadc742178ee5b8ba24e3c56e43bff149
anurag <anuragdw0710@gmail.com>
2024-12-18 22:17:59 +0530

    First

---
+++ app/src/main/java/com/example/myquizapplication/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myquizapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.main_layout)
+        val btn_start = findViewById<Button>(R.id.btn_start)
+        val et_name = findViewById<EditText>(R.id.et_name)
+        btn_start.setOnClickListener{
+            if(et_name.text.isEmpty()){
+                Toast.makeText(this,"Please give a name",Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this,QuizQuestionActivity::class.java)
+                startActivity(intent)
+
+            }
+        }
+    }
+}
+
